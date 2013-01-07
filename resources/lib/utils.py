d29c1698950d10868ff3c3607171659d619d7653
belese <belese@belese-VPCEB3S1E.(none)>
2013-01-07 11:09:21 +0100

    add wireless (not tested)

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -1,4 +1,5 @@
 import xbmc,xbmcgui
+from xbianconfig import xbianConfig
 
 def getNumeric(header,default=None,min=False,max=False):
     dialog = xbmcgui.Dialog() 
@@ -27,3 +28,55 @@ def getText(header,default=None):
         return kb.getText()
     else :
         return None
+
+def wifiConnect(interface,ssid,mode):
+	dialog = xbmcgui.Dialog()
+	networklist = xbianConfig('network','scan',interface)
+	displaylist = []
+	for ssid in networklist :
+		ssiddetails = ssid.split(',')
+		#check if hidden ssid
+		if ssiddetails[0] == '' :
+			name = 'hidden'
+		else :
+			name =  ssiddetails[0]
+		
+		#check security
+		if ssiddetails[1] == 'on' :
+			name += ' (%s)'%ssiddetails[2]
+		displaylist.append(name)
+	rc = dialog.select('Select Network',displaylist)
+	cont = True
+	if rc != -1 :
+		selectSsid = networklist[rc].split(',') 
+		#if hidden, ask ssid
+		if selectSsid[0] == '' :
+			name = getText('ssid')
+			if not name :
+				cont = False
+		else :
+			name =  selectSsid[0]
+		
+		#if not open, ask key :
+		if selectSsid[1] == 'on' and cont :
+			key = getText('Network Key')
+			if not key :
+				cont = False
+			else :
+				security = selectSsid[2]
+		elif selectSsid[1] == 'off' :
+			security = 'Open'
+			key = ''
+	else :
+		cont = False	
+		
+	if cont :
+		rc = xbianConfig('network','credentials',interface,security,name,key)
+		if rc == '1' :
+			return '%s (%s)'%(name,security) 
+		
+	
+	return '%s (%s)'%(ssid,mode)
+		
+		
+	
