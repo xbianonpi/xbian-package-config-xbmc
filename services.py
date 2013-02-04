38eb8244d16c13928e4bf51b0306d470ce6bcdc1
belese <belese@belese-VPCEB3S1E.(none)>
2013-02-04 12:07:05 +0100

    add resizedsd

--- services.py
+++ services.py
@@ -4,3 +4,7 @@ import xbmcgui
 from services.upgrade import upgrade
 upgrade_thread = upgrade()     
 upgrade_thread.onStart()
+
+from services.resize import resize
+resize_thread = resize()     
+resize_thread.onStart()
