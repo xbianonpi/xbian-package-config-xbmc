22541588cf404005126b828229835e8c7d102602
belese <belese@belese-VPCEB3S1E.(none)>
2013-02-05 12:13:56 +0100

    add firstrun service

--- services.py
+++ services.py
@@ -1,6 +1,11 @@
 import xbmc
 import xbmcgui
 
+
+from services.firstrun import firstrun
+firstrun_thread = firstrun()     
+firstrun_thread.onStart()
+
 from services.upgrade import upgrade
 upgrade_thread = upgrade()     
 upgrade_thread.onStart()
@@ -8,3 +13,4 @@ upgrade_thread.onStart()
 from services.resize import resize
 resize_thread = resize()     
 resize_thread.onStart()
+
