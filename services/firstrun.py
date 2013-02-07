d5b25f1fc7530e4a6590e5e2ed14262ea17ff1b2
belese <belese@belese-VPCEB3S1E.(none)>
2013-02-07 15:23:23 +0100

    fix first run

--- services/firstrun.py
+++ services/firstrun.py
@@ -2,20 +2,21 @@ import xbmcgui
 import xbmc
 from resources.lib.service import service
 from xbmcaddon import Addon
+import os
 
 __addonID__ = "plugin.xbianconfig"
 ADDON     = Addon( __addonID__ )
+ADDON_DATA  = xbmc.translatePath( "special://profile/addon_data/%s/" % __addonID__ )
 
 class firstrun(service):
     def onStart(self):
-        #check if first run
-        print 'fisrt run'
-        a = ADDON.getSetting('firstxbianrun')
-        if not a or a != 'true' :
-			#set default preference:
+        #check if first run        
+        firstlock = os.path.join(ADDON_DATA,'.firstrun')
+        if not os.path.isfile(firstlock) :
 			ADDON.setSetting('advancedmode','0')
 			ADDON.setSetting('notifyonerror','1')
 			ADDON.setSetting('notifyonsuccess','1')
-			ADDON.setSetting('confirmationonchange','1')			
-			xbmcgui.Dialog().ok('Welcome to XBian','Thanks to have choose xbian','bla bla bla')
-			ADDON.setSetting('firstxbianrun','true')
+			ADDON.setSetting('confirmationonchange','1')
+        	#set default preference:			
+			xbmcgui.Dialog().ok('Welcome to XBian','Thanks to have choose XBian','You can configure it, go to','System -> XBian')
+			open(firstlock,'w').close()			
