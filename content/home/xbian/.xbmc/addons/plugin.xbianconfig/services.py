5f61d51f0380c250d551c3c8d2f0f0a0af98f4b4
Matus Kral <matuskral@me.com>
2013-08-01 21:18:57 +0200

    removed resize service

--- content/home/xbian/.xbmc/addons/plugin.xbianconfig/services.py
+++ content/home/xbian/.xbmc/addons/plugin.xbianconfig/services.py
@@ -14,7 +14,3 @@ from services.upgrade import upgrade
 upgrade_thread = upgrade()     
 upgrade_thread.onStart()
 
-from services.resize import resize
-resize_thread = resize()     
-resize_thread.onStart()
-
