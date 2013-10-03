c02b1b18708f5431179e63d9a6fc78dc76304f7d
belese <besme2@gmail.com>
2013-10-03 18:55:16 +0200

    fix cancel in load dialog

--- content/home/xbian/.xbmc/addons/plugin.xbianconfig/default.py
+++ content/home/xbian/.xbmc/addons/plugin.xbianconfig/default.py
@@ -77,17 +77,20 @@ class xbian_config_python :
                 self.total = len(self.category_list)                
                 
                 for i,module in enumerate(self.category_list) :
-                    print 'Load %s'%module
-                    self.globalProgress =  int((float(self.finished)/(self.total)) * 100)
-                    self.update_progress(module.split('_')[1],'   initialise',0)                        
-                    catmodule = __import__('%s.%s'%(CATEGORY_PATH,module), globals(), locals(), [module])                        
-                    modu = getattr(catmodule,module.split('_')[1])                                                        
-                    catinstance = modu(self.CmdQueue,self.update_progress)                                        
-                    self.finished += 1                                        
-                    try :                    
-                       self.window.addCategory(catinstance)                            
-                    except:
-                       xbmc.log('XBian : Cannot add category: %s \n%s'%(str(module),str(sys.exc_info())))                                            
+                    if self.wait.iscanceled():
+						self.stop = True
+                    if not self.stop :
+                        print 'Load %s'%module
+                        self.globalProgress =  int((float(self.finished)/(self.total)) * 100)
+                        self.update_progress(module.split('_')[1],'   initialise',0)                        
+                        catmodule = __import__('%s.%s'%(CATEGORY_PATH,module), globals(), locals(), [module])                        
+                        modu = getattr(catmodule,module.split('_')[1])                                                        
+                        catinstance = modu(self.CmdQueue,self.update_progress)                                        
+                        self.finished += 1                                        
+                        try :                    
+                           self.window.addCategory(catinstance)                            
+                        except:
+                           xbmc.log('XBian : Cannot add category: %s \n%s'%(str(module),str(sys.exc_info())))                                            
                         
                 if not self.stop :
                     #self.wait.update(90,'Generate Windows')
