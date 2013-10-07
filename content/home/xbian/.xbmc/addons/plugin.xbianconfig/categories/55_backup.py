1126159060cab4481e5ece8d6d7433e768aa9df9
belese <besme2@gmail.com>
2013-10-07 22:00:32 +0200

    adjust to shell settings/finished gui

--- content/home/xbian/.xbmc/addons/plugin.xbianconfig/categories/55_backup.py
+++ content/home/xbian/.xbmc/addons/plugin.xbianconfig/categories/55_backup.py
@@ -7,165 +7,197 @@ from resources.lib.xbmcguie.category import Category,Setting
 
 from resources.lib.xbianconfig import xbianConfig
 from resources.lib.utils import *
-from xbmcaddon import Addon
 
-__addonID__      = "plugin.xbianconfig"
-ADDON     = Addon( __addonID__ )
-ADDON_DIR = ADDON.getAddonInfo( "path" )
-ROOTDIR            = ADDON_DIR
-BASE_RESOURCE_PATH = os.path.join( ROOTDIR, "resources" )
+import xbmcgui,xbmc
 
-
-BACKUP_PROFILE = ['Daily','Weekly','Monthly']
+BACKUP_PROFILE = ['Daily','Weekly']
 
 DEVICE = 'Device'
 FILE  = 'File'
 
-class systemBackup(Setting) :
-    CONTROL = CategoryLabelControl(Tag('label','Backup complete system (not functionnal yet)'))
+class homeBackupLabel(Setting) :
+    CONTROL = CategoryLabelControl(Tag('label','Home (Not Yet Implemented)'))
 
-class AutoBackup(MultiSettingControl):
+class systemBackupLabel(Setting) :
+    CONTROL = CategoryLabelControl(Tag('label','System (Not Yet Implemented)'))
+
+class systemBackup(MultiSettingControl):
     XBMCDEFAULTCONTAINER = False
 
     def onInit(self) :
-        self.backupType = SpinControlex(Tag('label','Source '))
-        self.addControl(self.backupType)        
-        contentS = Content(Tag('label','System'),defaultSKin=False)
-        self.backupType.addContent(contentS)
-        contentH = Content(Tag('label','Home'),defaultSKin=False)
-        self.backupType.addContent(contentH)        
-        
         #create the system settings
         #container with visibility condition
-        self.systemSettingsMulti = MultiSettingControl(Tag('visible','Container(%d).HasFocus(%d)'%(self.backupType.getWrapListId(),contentS.getId())))
- 
         #autoimage
         self.systemAutoBackup = RadioButtonControl(Tag('label','Auto image'))
-        self.systemSettingsMulti.addControl(self.systemAutoBackup)
-        self.systemAutoBackupMulti = MultiSettingControl(Tag('visible','SubString(Control.GetLabel(%d),*)'%self.systemAutoBackup.getId()))
-        self.systemSettingsMulti.addControl(self.systemAutoBackupMulti)
-        #Destination type (DEV/FILE) :
+        self.addControl(self.systemAutoBackup)
+        self.multiDelta = MultiSettingControl(Tag('visible','SubString(Control.GetLabel(%d),*)'%self.systemAutoBackup.getId()))
         self.systemdeltaControl = SpinControlex(Tag('label',' -type'))
         for number in BACKUP_PROFILE :
             self.systemdeltaControl.addContent(Content(Tag('label',number),defaultSKin=False))
-        self.systemAutoBackupMulti.addControl(self.systemdeltaControl)                  
-        self.systemBackupDestType = SpinControlex(Tag('label',' -Destination type '))
-        self.systemAutoBackupMulti.addControl(self.systemBackupDestType)        
+        self.multiDelta.addControl(self.systemdeltaControl)
+        self.addControl(self.multiDelta)
+        #Destination type (DEV/FILE) :
+        self.systemBackupDestType = SpinControlex(Tag('label','Destination type '))
+        self.addControl(self.systemBackupDestType)
         contentSD = Content(Tag('label',DEVICE),defaultSKin=False)
         self.systemBackupDestType.addContent(contentSD)
         contentSF = Content(Tag('label',FILE),defaultSKin=False)
-        self.systemBackupDestType.addContent(contentSF)                
-        self.systemdevicePath = ButtonControl(Tag('label',' -block device'),Tag('visible','Container(%d).HasFocus(%d) + Container(%d).HasFocus(%d)'%(self.systemBackupDestType.getWrapListId(),contentSD.getId(),self.backupType.getWrapListId(),contentS.getId())))
-        self.systemdevicePath.onClick = self.getDevice 
-        self.systemAutoBackupMulti.addControl(self.systemdevicePath)
-        self.systemfilePath = ButtonControl(Tag('label',' -Dest Name'),Tag('visible','Container(%d).HasFocus(%d) + Container(%d).HasFocus(%d)'%(self.systemBackupDestType.getWrapListId(),contentSF.getId(),self.backupType.getWrapListId(),contentS.getId())))
-        self.systemfilePath.onClick = lambda backupPath: self.backupPath.setValue(getFile('Backup Path',self.systemfilePath.getValue()))
-        self.systemAutoBackupMulti.addControl(self.systemfilePath)
-        
-        self.addControl(self.systemSettingsMulti)
-
-        #create the home settings
-        #container with visibility condition
-        self.homeSettingsMulti = MultiSettingControl(Tag('visible','Container(%d).HasFocus(%d)'%(self.backupType.getWrapListId(),contentH.getId())))
- 
-        #autoimage
-        self.homeAutoBackup = RadioButtonControl(Tag('label','Auto image'))
-        self.homeSettingsMulti.addControl(self.homeAutoBackup)
-        self.homeAutoBackupMulti = MultiSettingControl(Tag('visible','SubString(Control.GetLabel(%d),*)'%self.homeAutoBackup.getId()))
-        self.homeSettingsMulti.addControl(self.homeAutoBackupMulti)
-        #delta
-        self.homedeltaControl = SpinControlex(Tag('label',' -type'))
-        for number in BACKUP_PROFILE :
-            self.homedeltaControl.addContent(Content(Tag('label',number),defaultSKin=False))
-        self.homeAutoBackupMulti.addControl(self.homedeltaControl)                          
-        #dest
-        self.homefilePath = ButtonControl(Tag('label',' -Dest Name'))
-        self.homefilePath.onClick = lambda backupPath: self.homefilePath.setValue(getFile('Backup Path',self.homefilePath.getValue()))
-        self.homeAutoBackupMulti.addControl(self.homefilePath)
-        
-        self.addControl(self.homeSettingsMulti)
-
-
+        self.systemBackupDestType.addContent(contentSF)
+        self.systemdevicePath = ButtonControl(Tag('label',' -Block device'),Tag('visible','Container(%d).HasFocus(%d)'%(self.systemBackupDestType.getWrapListId(),contentSD.getId())))
+        self.systemdevicePath.onClick = lambda devicePath: self.systemdevicePath.setValue(self.getDevice())
+        self.addControl(self.systemdevicePath)
+        self.systemfilePath = ButtonControl(Tag('label',' -Dest folder'),Tag('visible','Container(%d).HasFocus(%d)'%(self.systemBackupDestType.getWrapListId(),contentSF.getId())))
+        self.systemfilePath.onClick = lambda backupPath: self.systemfilePath.setValue(getFile('Backup Path',self.systemfilePath.getValue()))
+        self.addControl(self.systemfilePath)
+        self.ManualBackup = ButtonControl(Tag('label','Start backup now'))
+        self.ManualBackup.onClick = lambda manualback : self.startManualBackup()
+        self.addControl(self.ManualBackup)
     def getDevice(self) :
         #to be override in gui - CB to display select dialog for ?UUID?
         pass
+
+    def startManualBackup(self) :
+        #to be override in gui - CB to startManualBackup
+        pass
+
     def setValue(self,value):
         print value
         #by default, display system auto backup
-        #        
-        #value = [     
-        # [
+        #
+        #value = [
         #  1,   #system backup enable
         #  'Device'/'File',   #system destination type - if change need to be modif on content
         #  'UUID'/'backup_dir', #system Path
         #  BACKUP_PROFILE[x]#system backup delta
-        # ],[ 
-        #  1, #home backup enable
-        #  'backup_dir', #backup home dir
-        #  BACKUP_PROFILE[x]#home backup profile
-        # ]] 
-        
-        #system
-        self.backupType.setValue('System')
-        self.systemAutoBackup.setValue(value[0][0])                        
-        self.systemBackupDestType.setValue(value[0][1])
+        # ]
+
+        self.systemAutoBackup.setValue(value[0])
+        self.systemBackupDestType.setValue(value[1])
         if value[1] == DEVICE :
-            self.systemdevicePath.setValue(value[0][2])
+            self.systemdevicePath.setValue(value[2])
         else :
-            self.systemfilePath.setValue(value[0][2])
-        self.systemdeltaControl.setValue(value[0][3])
-        #home
-        self.homeAutoBackup.setValue(value[1][0])
-        self.homefilePath.setValue(value[1][1])
-        self.homedeltaControl.setValue(value[1][2])   
+            self.systemfilePath.setValue(value[2])
+        self.systemdeltaControl.setValue(value[3])
+
+    def getValue(self) :
+        value = []
+        value.append(self.systemAutoBackup.getValue())
+        value.append(self.systemBackupDestType.getValue())
+        if value[-1] == DEVICE :
+            value.append(self.systemdevicePath.getValue())
+        else :
+            value.append(self.systemfilePath.getValue())
+        value.append(self.systemdeltaControl.getValue())
+        return value
+
 
 class AutoBackupGui(Setting) :
-    CONTROL = AutoBackup(Tag('visible','skin.hasSetting(advancedmode)'))
-    DIALOGHEADER = "Auto Backup"
-    ERRORTEXT = "Error updating"
+    CONTROL = systemBackup(Tag('visible','skin.hasSetting(advancedmode)'))
+    DIALOGHEADER = "System Backup"
+    ERRORTEXT = "Error during updating"
     OKTEXT = "Update ok"
-    SAVEMODE = Setting.ONUNFOCUS    
-        
-    def onInit(self) :
-        self.control.getDevice = self.getDevice
-    
-    def getDevice(self) :
-        #display a select dialog with UUID device and return it as a string.        
-        pass
-        
-    def getUserValue(self):
-        values =  self.control.getValue()
-        print 'Get user Value %s'%str(values)
-        return values
+    SAVEMODE = Setting.ONUNFOCUS
 
+    def onInit(self) :
+        #override CB
+        self.control.getDevice = self.getDevice
+        self.control.startManualBackup = self.startManualBackup
+
+    def startManualBackup(self):
+        dialog = xbmcgui.Dialog()
+        value = self.control.getValue()
+        if value[1] == 'File' :
+            value[2] = 'file://' + value[2]
+        
+        wait = dialogWait('Backing up System','Backup system to %s'%value[2])       
+        wait.show()     
+        #TODO
+        #test code
+        #do real backup here
+        i = 0
+        while True :
+            i += 1
+            time.sleep(1)
+            if i > 6 :
+                break
+        #end test code
+        wait.close()
+        xbmc.executebuiltin("Notification(%s,%s)"%('Backup System','Backup System is finished'))
+            
+            
+
+    def getDevice(self) :       
+        dialog = xbmcgui.Dialog()
+        #TODO
+        #get a list of uuid here (maybe with size to prevent error)
+        #will be a protection to not erase usb HDD with media?
+        uuid_list = ['fake UUID1','fake UUID2','fake UUID3','fake UUID4']
+        rc = dialog.select('Select Device',uuid_list)
+        if rc == -1 :
+           return self.xbianValue[2] #defaut device value in case of cancel
+        else :
+           return uuid_list[rc]
+
+    def getUserValue(self):
+        return self.control.getValue()
+        
     def getXbianValue(self):
-        #value = [     
-        # [
-        #  1,   #system backup enable
+        #value = [        
+        #  1/0,   #auto backup enable
         #  'Device'/'File',   #system destination type - if change need to be modif on content
         #  'UUID'/'backup_dir', #system Path
         #  BACKUP_PROFILE[x]#system backup delta
-        # ],[ 
-        #  1, #home backup enable
-        #  'backup_dir', #backup home dir
-        #  BACKUP_PROFILE[x]#home backup profile
-        # ]] 
+        # ]
+        
         #TODO
         #read default Value from file here
-        #value is like [1, '/home/belese/', 'Daily']
-        print 'Get xbian Value'
-        #self.control.setEnabled(False)
-        return [[1,'Device','FAKEUUID',BACKUP_PROFILE[0]],[0,'/temp',BACKUP_PROFILE[1]]]
+        #value is like [1,'File','/home/belese/','Daily']        
+        return [1,'Device','FAKEUUID',BACKUP_PROFILE[0]]
 
     def setXbianValue(self,value):
+        #value is like [1,'File','/home/belese/', 'Daily']
+        # or [1,'Device','UUID','Daily']        
         #TODO
-        #save xbian value in file here
-        #value is like [1, '/home/belese/', 'Daily']
+        #save xbian autobackup values here
         #return True if ok, False either
         print 'Save xbian :%s'%value
         return True
 
+class homeBackup(Setting) :
+    CONTROL = ButtonControl(Tag('label','Start Backup Now'))
+    DIALOGHEADER = "Home Backup"
+    ERRORTEXT = "Error during backup home"
+    OKTEXT = "Home Backup is finished"
+
+    def setControlValue(self,value) :
+        pass
+
+    def getUserValue(self):
+        dialog = xbmcgui.Dialog()
+        wait = dialogWait('Backing up Home','Home backup will be available on your samba share or ???')        
+        wait.show()     
+        #test code
+        #TODO
+        #do real backup here
+        i = 0
+        while True :
+            i += 1
+            time.sleep(1)
+            if i > 6 :
+                break
+        #end test code
+        wait.close()
+        xbmc.executebuiltin("Notification(%s,%s)"%('Backup Home','Backup Home is finished'))
+        return ''
+
+    def getXbianValue(self):
+        return ''
+
+    def setXbianValue(self,value):
+        return ok
+
+
 class backup(Category) :
     TITLE = 'Backup'
-    SETTINGS = [systemBackup,AutoBackupGui]
+    SETTINGS = [homeBackupLabel,homeBackup,systemBackupLabel,AutoBackupGui]
 
