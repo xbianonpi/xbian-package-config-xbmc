18e60c48b2b58749a75dea0549b13a4da12fd214
Matus Kral <matuskral@me.com>
2013-10-14 01:38:17 +0200

    autoimg button problem solved.

--- content/home/xbian/.xbmc/addons/plugin.xbianconfig/categories/55_backup.py
+++ content/home/xbian/.xbmc/addons/plugin.xbianconfig/categories/55_backup.py
@@ -180,8 +180,14 @@ class AutoBackupGui(Setting) :
         if dest :
             dest = dest[0]
         else :
-            dest = ''   
-        return [0,imgtype,dest,delta]
+            dest = ''
+
+        if delta != 'none' :
+            autoimg = 1 
+        else :
+            autoimg = 0
+
+        return [autoimg,imgtype,dest,delta]
 
     def setXbianValue(self,value):
         #value is like [1,'File','/home/belese/', 'Daily']
