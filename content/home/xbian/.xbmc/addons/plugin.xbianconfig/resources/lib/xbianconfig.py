40ade30499e304dedfb328e6a1a48b2e64a3d00a
belese <besme2@gmail.com>
2013-10-17 21:50:04 +0200

    allow external script

--- content/home/xbian/.xbmc/addons/plugin.xbianconfig/resources/lib/xbianconfig.py
+++ content/home/xbian/.xbmc/addons/plugin.xbianconfig/resources/lib/xbianconfig.py
@@ -4,9 +4,10 @@ import xbmc
 def sh_escape(s):
    return s.replace("(","\\(").replace(")","\\)").replace(" ","\\ ")
 
-def xbianConfig(*args):
-        cmd = ['sudo','/usr/local/sbin/xbian-config']
+def xbianConfig(*args,**kwargs) :
+        cmd = kwargs.get('cmd',['sudo','/usr/local/sbin/xbian-config'])
         for arg in args :
+           print arg
            cmd.append(sh_escape(arg))
         rc= subprocess.check_output(cmd)
         rcs = rc.split('\n')
