1fbd05e22bfa0c3261227dd0057087d53928b270
belese <besme2@gmail.com>
2013-10-22 01:24:25 +0200

    optimisation : keep bash session open and communicate via stdin/out

--- content/home/xbian/.xbmc/addons/plugin.xbianconfig/resources/lib/xbianconfig.py
+++ content/home/xbian/.xbmc/addons/plugin.xbianconfig/resources/lib/xbianconfig.py
@@ -1,17 +1,29 @@
 import subprocess
 import xbmc
 
+import threading
+
+xbmc.log('XBian-config : Initialise bash sessions',xbmc.LOGDEBUG)
+process = subprocess.Popen(['/bin/bash'], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+processlock = threading.Lock()
+
 def sh_escape(s):
    return s.replace("(","\\(").replace(")","\\)").replace(" ","\\ ")
 
-def xbianConfig(*args,**kwargs) :
-        cmd = kwargs.get('cmd',['sudo','/usr/local/sbin/xbian-config'])
+def xbianConfig(*args,**kwargs) :        
+        cmd = kwargs.get('cmd',['sudo','/usr/local/sbin/xbian-config'])        
         for arg in args :           
-           cmd.append(sh_escape(arg))
-        rc= subprocess.check_output(cmd)
-        rcs = rc.split('\n')
-        result  = filter(lambda x: len(x)>0, rcs)
-        xbmc.log('XBian : xbian-config %s : %s '%(' '.join(cmd[2:]),str(result)),xbmc.LOGDEBUG)
+           cmd.append(sh_escape(arg))        
+        processlock.acquire()        
+        process.stdin.write(' '.join(cmd) + ' ; echo EndCall\n')        
+        rcs = []
+        while True :			
+			line = process.stdout.readline()[:-1]
+			if line == 'EndCall':
+				processlock.release()
+				break
+			rcs.append(line)
+        result  = filter(lambda x: len(x)>0, rcs)        
+        xbmc.log('XBian-config : xbian-config %s : %s'%(' '.join(cmd[2:]),str(result)),xbmc.LOGDEBUG)        
         return result
         
-
