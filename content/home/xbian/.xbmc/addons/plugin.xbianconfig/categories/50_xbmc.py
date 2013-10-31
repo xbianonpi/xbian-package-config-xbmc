d3a7eff0e7ec7e089c012c7b6a2644c71a910a02
Matus Kral <matuskral@me.com>
2013-10-31 05:23:31 +0100

    turn on TV when XBMC exits ver up

--- content/home/xbian/.xbmc/addons/plugin.xbianconfig/categories/50_xbmc.py
+++ content/home/xbian/.xbmc/addons/plugin.xbianconfig/categories/50_xbmc.py
@@ -84,6 +84,33 @@ class xbmcTvOffGui(Setting) :
         else :
             return False
 
+class xbmcTvOn(Setting) :
+    CONTROL = RadioButtonControl(Tag('label','Turn CEC capable TV on with XBMC exit'))
+    DIALOGHEADER = "TV ON exit"
+    ERRORTEXT = "Error on updating"
+    OKTEXT = "Update ok"
+
+    def getUserValue(self):
+        return str(self.getControlValue())
+
+    def setControlValue(self,value) :
+        if value == '1' :
+            value = True
+        else :
+            value = False
+        self.control.setValue(value)
+
+    def getXbianValue(self):
+        rc =xbianConfig('xbmc','tvexiton')
+        return rc[0]
+
+    def setXbianValue(self,value):
+        rc =xbianConfig('xbmc','tvexiton',str(value))
+        if rc and rc[0] == '1' :
+            return True
+        else :
+            return False
+
 class xbmcUSBLabel(Setting) :
     CONTROL = CategoryLabelControl(Tag('label','USB HotPlug'))
 
@@ -308,4 +335,4 @@ class xbmcUSBsync(Setting) :
 #CATEGORY CLASS
 class xbmc(Category) :
     TITLE = 'XBMC'
-    SETTINGS = [xbmcLabel,xbmcGui,xbmcTvOffGui,DynamicPriorityGui,xbmcUSBLabel,xbmcUSBmount,xbmcUSBsync,xbmcUSBsmb,xbmcUSBsmbrw,xbmcUSBuuidname,SpinDownHddGui]
+    SETTINGS = [xbmcLabel,xbmcGui,xbmcTvOffGui,xbmcTvOn,DynamicPriorityGui,xbmcUSBLabel,xbmcUSBmount,xbmcUSBsync,xbmcUSBsmb,xbmcUSBsmbrw,xbmcUSBuuidname,SpinDownHddGui]
