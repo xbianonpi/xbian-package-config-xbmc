07a89bd894b8cc3d8d43addb1fea49bf533dd7b6
belese <besme2@gmail.com>
2013-11-01 17:03:55 +0100

    fix : quoteplus addons params

--- content/home/xbian/.xbmc/addons/plugin.xbianconfig/resources/lib/xbianWizardDialog.py
+++ content/home/xbian/.xbmc/addons/plugin.xbianconfig/resources/lib/xbianWizardDialog.py
@@ -1,12 +1,13 @@
 import xbmcgui
 import wizard
+import urllib
 
 class wizardDialog(xbmcgui.WindowXMLDialog) :
     def onInit(self) :        
         self.mainCtrl = self.getControl(50000)
         for cate in wizard.__all__ :
             setting = __import__('%s.%s'%('wizard',cate), globals(), locals(), [cate])
-            data = getattr(setting,'DATA')            
+            data = getattr(setting,'DATA')                        
             self.addWizard(data['smalltitle'],data['title'],data['description'],data['action'])
         
     def addWizard(self,smallTitle,title,description,action) :
@@ -15,7 +16,8 @@ class wizardDialog(xbmcgui.WindowXMLDialog) :
         item.setProperty('title',title)
         item.setLabel2(description)            
         path = 'None'
+        print action
         if action != None :
-             path = 'mode=1&title=%s&settings=%s'%(smallTitle,",".join(action))
+             path = 'mode=1&title=%s&settings=%s'%(urllib.quote_plus(smallTitle),urllib.quote_plus(",".join(action)))
         item.setProperty('path',path)
         self.mainCtrl.addItem(item)            
