ac356f5be3320d965c83d54787df6ec922b965e4
belese <besme2@gmail.com>
2013-11-01 14:26:01 +0100

    add xbianDialog (list dialog for xbian settings)

---
+++ content/home/xbian/.xbmc/addons/plugin.xbianconfig/resources/lib/xbianDialog.py
@@ -0,0 +1,55 @@
+import traceback
+from xbmcguie.window import DialogSkinXml
+import threading
+import xbmc
+
+class XbianDialog(DialogSkinXml):
+    def init(self) :
+        xbmc.log('XBian-config : Init XbianDialog',xbmc.LOGDEBUG)
+        self.settings = []        
+        self.stopRequested = False
+        self.title = ""
+        self.queue = None
+        
+    def initialise(self,title,queue) :
+        self.title = title
+        self.queue = queue
+        
+    def onInit(self):
+        xbmc.log('XBian-config : Show(onInit) XbianDialog',xbmc.LOGDEBUG)
+        for setting in self.settings :
+            setting.addQueue(self.queue)       
+            self.addControl(setting.getControl())        
+        DialogSkinXml.onInit(self)
+        #set title
+        self.getControl(20).setLabel(self.title)        
+        for setting in self.settings :       
+            self._loadSettingValue(setting)
+        xbmc.log('XBian-config : End Show(onInit) XbianDialog',xbmc.LOGDEBUG) 
+               
+    def _loadSettingValue(self,setting):                  
+        try :           
+           setting.updateFromXbian()                      
+        except :
+                 #don't enable control if error                 
+            xbmc.log('XBian-config : Not catched exception(Get xbianValue) : %s - Settings will stay disabled'%(traceback.format_exc()),xbmc.LOGERROR)                                  
+        else :
+            setting.getControl().setEnabled(True)                  
+        
+    def addSetting(self,setting):
+        self.settings.append(setting())        
+        
+
+    def doXml(self,template) :
+        xbmc.log('XBian-config : Generate dialog xml from template : %s to %s'%(template,self.xmlfile),xbmc.LOGDEBUG)
+        xmltemplate = open(template)
+        xmlout = open(self.xmlfile,'w')
+        for line in xmltemplate.readlines() :
+            if '<control type="xbian" value="settings"/>' in line :
+                for setting in self.settings :
+                    xmlout.write(setting.getControl().toXml())                    
+            else :
+                xmlout.write(line)
+        xmltemplate.close()
+        xmlout.close()
+        xbmc.log('XBian-config : End Generate windows xml',xbmc.LOGDEBUG)
--- content/home/xbian/.xbmc/addons/plugin.xbianconfig/resources/lib/xbmcguie/window.py
+++ content/home/xbian/.xbmc/addons/plugin.xbianconfig/resources/lib/xbmcguie/window.py
@@ -9,19 +9,16 @@ ADDON     = Addon( __addonID__ )
 ADDON_DIR = ADDON.getAddonInfo( "path" )
 ROOTDIR            = ADDON_DIR
 
-SKIN_DIR = xbmc.getSkinDir()
-
 ACTION_PREVIOUS_MENU = 10
 ACTION_BACK = 92
 
-try:
-   with open(os.path.join(ROOTDIR,'resources','skins',SKIN_DIR,'720p','SettingsXbianInfo.template')): pass
-except IOError:
-   SKIN_DIR = 'Default'
-
-
 class WindowSkinXml(xbmcgui.WindowXML):   
     def __init__(self,strXMLname, strFallbackPath, strDefaultName=False, forceFallback=False):
+        SKIN_DIR = xbmc.getSkinDir()
+        try:
+            with open(os.path.join(ROOTDIR,'resources','skins',SKIN_DIR,'720p','SettingsXbianInfo.template')): pass
+        except IOError:
+            SKIN_DIR = 'Default'
         self.xmlfile = os.path.join(strFallbackPath,'resources','skins',SKIN_DIR,'720p',strXMLname)
         self.controls = []
         self.init()
@@ -49,16 +46,49 @@ class WindowSkinXml(xbmcgui.WindowXML):
         self.onHeritFocus(controlID)
     
     def onAction(self,Action) :
-		if Action in (ACTION_PREVIOUS_MENU,ACTION_BACK) :
-			self.close()
-		self.onHeritAction(Action)
-		
+        if Action in (ACTION_PREVIOUS_MENU,ACTION_BACK) :
+            self.close()
+        self.onHeritAction(Action)
+        
     def onHeritAction(self,Action) :
-		print 'super onheritaction%s'%str(Action)
-		#could be herit on real window
-		pass
+        print 'super onheritaction%s'%str(Action)
+        #could be herit on real window
+        pass
     
     def onHeritFocus(self,controlID) :
-		#could be herit on real window
-		pass
+        #could be herit on real window
+        pass
+        
+
+class DialogSkinXml(xbmcgui.WindowXMLDialog):   
+    def __init__(self,strXMLname, strFallbackPath, strDefaultName=False, forceFallback=False):
+        SKIN_DIR = xbmc.getSkinDir()
+        try:
+            with open(os.path.join(ROOTDIR,'resources','skins',SKIN_DIR,'720p','SettingsXbianDialog.template')): pass
+        except IOError:
+            SKIN_DIR = 'Default'
+        self.xmlfile = os.path.join(strFallbackPath,'resources','skins',SKIN_DIR,'720p',strXMLname)
+        self.controls = []
+        self.init()
+    
+    def onInit(self):
+        #set the windows instance in all xbmc control
+        for control in self.controls :
+            control.setWindowInstance(self)
+    
+    def doXml(self,template) :
+        #must override this function
+        #must create the xml
+        pass
+            
+    def addControl(self,control):        
+        self.controls.append(control)
+        
+    def onClick(self, controlID):        
+        for control in self.controls :
+            control.click(controlID)
+ 
+    def onFocus(self, controlID):        
+        for control in self.controls :
+            control.focus(controlID)        
         
