cc47ba4e58344c22e00227c97a26611ae3453423
belese <besme2@gmail.com>
2013-11-11 14:26:57 +0100

    typo

--- content/home/xbian/.xbmc/addons/plugin.xbianconfig/wizard/30_license.py
+++ content/home/xbian/.xbmc/addons/plugin.xbianconfig/wizard/30_license.py
@@ -1,6 +1,6 @@
 DATA={
 "title" : "Enter your License key",
 "smalltitle" : "License key",
-"description" : 'The Raspberry Pi can decode the majority of video and audio formats, however, in order to decode VC-1 (Used in some BluRay discs) and MPEG-2) you will need to purchase a license key for these.[CR]They are available at http://www.raspberrypi.com/license-keys/[CR][CR]If you already have a license key, you can enter it now.',
+"description" : 'The Raspberry Pi can decode the majority of video and audio formats, however, in order to decode VC-1 (Used in some BluRay discs) and MPEG-2, you will need to purchase a license key for these.[CR]They are available at http://www.raspberrypi.com/license-keys/[CR][CR]If you already have a license key, you can enter it now.',
 "action" : ['categories.10_system.vc1License','categories.10_system.mpeg2License']
 }
