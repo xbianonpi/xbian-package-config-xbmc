fe4f35878b4d751315658f7454f6fc392d134dae
Matus Kral <matuskral@me.com>
2013-11-13 03:18:41 +0100

    small updates ver up

--- content/home/xbian/.xbmc/addons/plugin.xbianconfig/wizard/40_backup.py
+++ content/home/xbian/.xbmc/addons/plugin.xbianconfig/wizard/40_backup.py
@@ -1,6 +1,6 @@
 DATA={
 "title" : "Restore Home",
 "smalltitle" : "Restore",
-"description" : 'XBian will offer you an easy way to backup your setup.[CR]If you have a previous backup of your home[CR](it contain your info, like xbmc addons, parameters,...),[CR]you can restore now.[CR][CR]Later, if you want to do a backup, create a snapshot and many more,[CR],go to system->xbian->backup',
+"description" : 'XBian will offer you an easy way to backup your setup.[CR]If you have a previous backup of your home[CR](it contain your XBMC configuration, addons, settings including Libraries...),[CR]you can restore now.[CR][CR]Later, if you want to do a backup, create a snapshot and many more,[CR],go to system->xbian->backup',
 "action" : ['categories.55_backup.homeRestoreBackup']
 }
