e626f8eb2d63f3f354d5e9329d0227c8deadf888
anaconda <anaconda@menakite.eu>
2014-06-20 02:15:43 +0200

    [services] Drop xbmcloaded "service".
    This was moved to the "Upstart bridge" add-on with commit
    anaconda/xbian-package-upstart-xbmc-bridge@03d4ebbcfb8a6f0d873bd14fa8aaec241913c6cd.

--- content/usr/local/share/xbmc/addons/plugin.xbianconfig/services/xbmcloaded.py
+++
@@ -1,7 +0,0 @@
-from resources.lib.service import service
-import os
-
-class reportloaded(service):
-    def onStart(self):
-        os.system("/usr/bin/sudo /sbin/start -n xbmc-loaded")
-        print 'XBian : notify upstart'
