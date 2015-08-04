2a03de8e377b9f1ef83688c9f84d4d1cc6eaf625
Thiago Bellini <hackedbellini@gmail.com>
2015-08-04 10:26:43 -0300

    Improve directory structure and build script
    * Move the plugin source to root. The tree structure is only needed to build
      the script with `dpkg-deb`, so we only need it when running the script.
    
    * Fix gen.package.sh script to consider those changes and remove some
      unnecessary code from it.
    
    * Put debian directory on root too. Also, avoid storing the duplicated
      we generate for `dpkg-deb`. We only need it to build the package.
      Also, this is a step forward in direction to stop using `dpkg-deb` and
      use a real build tool instead (debuilder or even xbian's build tool).
    
    * Set myself as the maintainer of the deb package

--- gen.package.sh
+++ gen.package.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-rm_size() {
-	cat ./content/DEBIAN/control | grep -v "Installed-Size:" > ./content/DEBIAN/control.new
-	mv ./content/DEBIAN/control.new ./content/DEBIAN/control
-	rm -f ./size.txt
-}
+BUILD_DIR=build
+INSTALL_DIR=usr/local/share/kodi/addons/plugin.xbianconfig
+ADDON_DIR=xbianconfig
 
 str='strip'
 strargs='--strip-unneeded'
@@ -18,30 +16,31 @@ if ! dpkg-architecture -iarmhf; then
     [ -z "$str" ] && { echo "please install binutils-arm-linux-gnueabihf on Debian or arm-linux-gnu-strip on CentOS"; str=''; true; }
 fi
 
-package=$(cat ./content/DEBIAN/control | grep Package | awk '{print $2}')
-version1=$(cat ./content/DEBIAN/control | grep Version | awk '{print $2}' | awk -F'-' '{print $1}')
-version2=$(cat ./content/DEBIAN/control | grep Version | awk '{print $2}' | awk -F'-' '{print $2}')
+# Make sure to remove build dir after building the package
+trap "rm -rf $BUILD_DIR" EXIT SIGINT SIGTERM
 
-[ $(date +%Y%m%d) = $version1 ] && version2=$((version2 + 1)) || version2=0
+mkdir -p $BUILD_DIR/$INSTALL_DIR
+mkdir -p $BUILD_DIR/DEBIAN
+cp -r $ADDON_DIR/* $BUILD_DIR/$INSTALL_DIR
+cp -r debian/* $BUILD_DIR/DEBIAN
+
+find $BUILD_DIR/$INSTALL_DIR -name "*.pyc" -delete
+
+package=$(cat debian/control | grep Package | awk '{print $2}')
 version1=$(date +%Y%m%d)
-
-version=$version1-$version2
-for f in $(find ./content-tpl -type f -printf "%P\n"); do
-    cp ./content-tpl/$f content/$f
-    sed -i "s%__DATE__%$version%g" ./content/$f
+version2=0
+while [ -f "$package-$version1-$version2.deb" ]; do
+    version2=$(($version2 + 1))
 done
+version=$version1-$version2
+sed -i "s%__DATE__%$version%g" $BUILD_DIR/DEBIAN/control
 
-# calculate size dynamically. remove first any entry, then add the actual 
-rm_size
-
-cd content
-[ -d ./tmp ] && mv ./tmp ..
+cd $BUILD_DIR
 find ./ -type f -print0 | xargs -0 -L1 printf "%s\n" | while read f; do [ -z "$str" ] || $str $strargs "$f" 2>/dev/null; s=$(stat -c %s "$f"); tot=$((tot + (s/1024)+1)); echo $tot > ../size.txt; done
 printf "Installed-Size: %u\n" $(cat ../size.txt) >> ./DEBIAN/control
+rm ../size.txt
 find ./ -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P\0' | sort -z| xargs --null md5sum > DEBIAN/md5sums
 cd ..
-fakeroot dpkg-deb -b ./content "${package}""${version}".deb
-[ -d ./tmp ] && mv ./tmp ./content
-# remove the size again, because on different filesystems du will return different size
-rm_size
-#rm -f ./content/etc/default/*.default
+
+# FIXME: We should really build the package instead of dpkg-deb here
+fakeroot dpkg-deb -b $BUILD_DIR "$package-$version.deb"
