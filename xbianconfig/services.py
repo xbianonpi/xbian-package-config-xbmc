6d0809b5e86ed432e5b69fb435c08f0569270c55
mkreisl <manfred.kreisl@nexgo.de>
2017-04-23 18:59:21 +0200

    Rework of resident part and adding new features: - Polling of events replaced by usage of kernel's inotify feature - Reload config settings if necessary - Manually trigger of refresh package database added by create/modify file /run/force-refresh - Simple message interface added via files, background can send messages which will be displayed   in 4 line message box. File /run/splash/msg4kodi for volatile messages and   /home/xbian/.kodi/userdata/addon_data/plugin.xbianconfig/msg4kodi for reboot resident messages   will be processed   Format of message file like this:   Message 1 Text for line 1   Message 1 Text for line 2 (optional)   Message 1 Text for line 3 (optional)   Message 1 Text for line 4 (optional)   $   Message 2 Text for line 1   Message 2 Text for line 2 (optional)   Message 2 Text for line 3 (optional)   Message 2 Text for line 4 (optional)   $   or for predefined Messages which can be localized:   #001   for message number 1
    - New function(s):
    
      1) Hints can be displayed and can be deactivated. Currently implemented:
         xbiancopy:     Message when system backup has not be done for long time
         backuphome:    Message when /home backup has not be done for long time
         updates:       Message, when updates are available to install
         upgradenotify: Message, when distribution upgrade is available
    
      2) Message interface, see above
    
      3) Manually package database refresh can be forced, can be done per cron job.
         For example entry int crontab
         28 22  * * 1 /bin/date \+\%F\-\%H\%M > /run/force-refresh
         will start refresh db 22:28 every Monday

--- xbianconfig/services.py
+++ xbianconfig/services.py
@@ -1,15 +1,123 @@
 import os
+import xbmc
+import pyinotify
 
 from services.firstrun import firstrun
-from services.upgrade import upgrade
+from services.xbianworker import xbianworker
 
+print 'XBian services started (%s)' % xbmc.__version__
+
+__addonID__ = "plugin.xbianconfig"
+
+# Files and/or folders which are be watched by inotify
+PERIODICSETTING = '/etc/apt/apt.conf.d/10xbian-periodic'
+UPDATEREPO      = '/usr/local/sbin/xbian-update-repo'
+RUNPATH         = '/run'
+REBOOTREQUIRED  = 'reboot-required'
+FORCEREFRESH    = 'force-refresh'
+SETTINGPATH     = '/etc/default'
+
+# Folder we write/expect status/config files
+MASTERPATH      = xbmc.translatePath("special://userdata/addon_data/%s/" % __addonID__)
+ADDONPATH       = xbmc.translatePath("special://profile/addon_data/%s/" % __addonID__)
+
+# Statusfiles from backup image and home folder process
+XBIANCOPY       = 'xbiancopy'
+BACKUPHOME      = 'backuphome'
+
+IMAGESTATUS     = os.path.join(MASTERPATH, XBIANCOPY)
+HOMESTATUS      = os.path.join(MASTERPATH, BACKUPHOME)
+
+MSG4KODI        = 'msg4kodi'
+MSG4KODIFILE1   = os.path.join(MASTERPATH, MSG4KODI)
+MSG4KODIFILE2   = os.path.join('/run/splash', MSG4KODI)
 
 onRun = os.path.join('/', 'tmp', '.xbian_config_python')
 if os.path.isfile(onRun):
     os.remove(onRun)
+onWiz = os.path.join('/', 'tmp', '.xbian_wizard')
+if os.path.isfile(onWiz):
+    os.remove(onWiz)
 
-firstrun_thread = firstrun()
-firstrun_thread.onStart()
+firstrun().onStart()
 
-upgrade_thread = upgrade()
-upgrade_thread.onStart()
+worker = xbianworker()
+
+class eventHandler(pyinotify.ProcessEvent):
+
+    def process_IN_CREATE(self, event):
+        print event
+        if event.name == REBOOTREQUIRED:
+            worker.onStatusChanged('reboot')
+        elif event.name == FORCEREFRESH:
+            worker.onStatusChanged('refresh')
+
+    def process_IN_DELETE(self, event):
+        print event
+        if event.name == REBOOTREQUIRED:
+            worker.onStatusChanged('noreboot')
+        elif 'hide.' in event.name:
+            worker.onStatusChanged(event.name)
+
+    def process_IN_MODIFY(self, event):
+        print event
+        if event.pathname == PERIODICSETTING or event.name == 'xbian-snap' or event.name == 'notifywhenbusy':
+            worker.onStatusChanged('setting')
+        elif event.name == MSG4KODI:
+            worker.onStatusChanged(MSG4KODI, event.pathname)
+        elif event.pathname == UPDATEREPO:
+            worker.onStatusChanged('upgrade')
+        elif event.pathname == IMAGESTATUS:
+            worker.onStatusChanged(XBIANCOPY, IMAGESTATUS)
+        elif event.pathname == HOMESTATUS:
+            worker.onStatusChanged(BACKUPHOME, HOMESTATUS)
+        elif 'hide.' in event.name:
+            worker.onStatusChanged(event.name)
+        elif event.name == REBOOTREQUIRED:
+            worker.onStatusChanged('reboot')
+        elif event.name == FORCEREFRESH:
+            worker.onStatusChanged('refresh')
+
+    def process_IN_MOVED_TO(self, event):
+        print event
+        if event.pathname == PERIODICSETTING or event.name == 'xbian-snap':
+            worker.onStatusChanged('setting')
+
+wm = pyinotify.WatchManager()
+
+notifier = pyinotify.ThreadedNotifier(wm, eventHandler())
+
+wm.add_watch(SETTINGPATH, pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY | pyinotify.IN_MOVED_TO)
+wm.add_watch(RUNPATH, pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY | pyinotify.IN_MOVED_TO)
+wm.add_watch(MASTERPATH, pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY)
+if MASTERPATH != ADDONPATH:
+    print 'XBian : add profile path %s' % ADDONPATH
+    wm.add_watch(ADDONPATH, pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY)
+wm.add_watch('/run/splash', pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY)
+wm.watch_transient_file(PERIODICSETTING, pyinotify.IN_MODIFY, eventHandler)
+wm.watch_transient_file(UPDATEREPO, pyinotify.IN_MODIFY, eventHandler)
+
+notifier.start()
+
+if os.path.isfile(os.path.join(RUNPATH, REBOOTREQUIRED)):
+    worker.onStatusChanged('reboot')
+
+if os.path.isfile(UPDATEREPO):
+    worker.onStatusChanged('upgrade')
+
+if os.path.isfile(MSG4KODIFILE1):
+    worker.onStatusChanged(MSG4KODI, MSG4KODIFILE1)
+
+if not os.path.isfile(IMAGESTATUS):
+    open(IMAGESTATUS, 'w').close()
+worker.onStatusChanged(XBIANCOPY, IMAGESTATUS)
+
+if not os.path.isfile(HOMESTATUS):
+    open(HOMESTATUS, 'w').close()
+worker.onStatusChanged(BACKUPHOME, HOMESTATUS)
+
+worker.onStart()
+
+notifier.stop()
+
+print 'XBian services finished'
--- xbianconfig/services/xbianworker.py
+++ xbianconfig/services/xbianworker.py
@@ -13,30 +13,110 @@ _ = resources.lib.translation.language.gettext
 __addonID__ = "plugin.xbianconfig"
 ADDON = Addon(__addonID__)
 
+#
+# Predefined messages for external programs
+#
+msgs = [
+  [ _('Update notification'), '', _('Resident part of package xbmc-config has been changed'), _('A restart of Kodi is recommended') ],
+  [ _('Update notification'), '', _('Package xbmc has been updated'), _('You have to restart Kodi immediately') ],
+  [ _('Update notification'), '', _('Package upstart-xbmc-bridge has been updated'), _('A restart of Kodi is recommended') ],
+  [  '4.1',  '4.2',  '4.3' ],
+  [  '5.1',  '5.2',  '5.3' ],
+  [  '6.1',  '6.2',  '6.3' ],
+  [  '7.1',  '7.2',  '7.3' ],
+  [  '8.1',  '8.2',  '8.3' ],
+  [  '9.1',  '9.2',  '9.3' ],
+  [ '10.1', '10.2', '10.3' ],
+]
 
-class upgrade(service):
+class xbianworker(service):
 
     def onInit(self):
         self.StopRequested = False
-        self.xbianUpdate = False
-        self.packageUpdate = False
-        self.rebootNeeded = False
-        self.rebootNoCheck = False
+        self.updatesAvailable = False
+        self.rebootPending = False
+        self.upgradePending = False
         self.inScreenSaver = False
-        self.selfCheck = False
+        self.forceRefresh = False
+        self.hideUpdatesAvailable = False
+        self.hideImageWarning = False
+        self.hideHomeWarning = False
+        self.hideUpgradeNotify = False
+        self.settingsUpdated = False
+        self.notifyWhenBusy = False
+        self.refreshNext = datetime.now() + timedelta(minutes=30)
+        self.xbiancopyDone = self.backuphomeDone = False
+        self.xbiancopyDate = self.backuphomeDate = None
+        self.timeImageDiff = self.timeHomeDiff = timedelta(minutes=30)
+        self.msgdisp = 0
+        self.msgcount = 10
+        self.msgheader = []
+        self.msgcontent = []
+        self.loop = 600
 
+        if getSetting('hide.updates') == 1:
+            self.hideUpdatesAvailable = True
+        if getSetting('hide.xbiancopy') == 1:
+            self.hideImageWarning = True
+        if getSetting('hide.backuphome') == 1:
+            self.hideHomeWarning = True
+        if getSetting('hide.upgradenotify') == 1:
+            self.hideUpgradeNotify = True
+
+        self.actualizeSettings()
+
+        while self.msgcount > 0:
+            self.msgheader.append('')
+            self.msgcontent.append([ '', '', '' ])
+            self.msgcount -= 1
+
+        print 'XBian : xbianworker service started'
+
+    def actualizeSettings(self, notify=False):
         rc = xbianConfig('updates', 'enableauto')
         if rc and rc[0] == '1':
             self.enableauto = True
         else:
             self.enableauto = False
+
         try:
-            self.deltaCheck = int(xbianConfig('updates', 'autoinventory')[0].split(' ')[1])
+            rc = xbianConfig('updates', 'autoinventory')
+            self.deltaCheck = int(rc[0].split(' ')[1])
+            if rc[0].split(' ')[0] == '0':
+                self.selfCheck = True
+            else:
+                self.selfCheck = False
+
         except:
-            self.deltaCheck = 100
+            self.deltaCheck = 1
             self.selfCheck = True
 
-        print 'XBian : upgrade service started'
+        self.timeImageDiff = self.calcTimedelta('imgplan', self.xbiancopyDone)
+        self.timeHomeDiff = self.calcTimedelta('homeplan', self.backuphomeDone)
+
+        if getSetting('notifywhenbusy') == '1':
+            self.notifyWhenBusy = True
+        else:
+            self.notifyWhenBusy = False
+
+        if notify:
+            self.settingsUpdated = True
+            self.loop = 10
+
+    def calcTimedelta(self, type, done):
+        if done:
+            delta = timedelta(days=31,hours=3)
+        else:
+            delta = timedelta(days=3)
+        planned = xbianConfig('xbiancopy', type)
+        if planned:
+            if planned[0] == 'daily':
+                delta = timedelta(hours=27)
+            elif planned[0] == 'weekly':
+                delta = timedelta(days=7,hours=3)
+            elif planned[0] == 'monthly':
+                delta = timedelta(days=31,hours=3)
+        return delta
 
     def onAbortRequested(self):
         print 'XBian : abort requested'
@@ -47,15 +127,34 @@ class upgrade(service):
         except:
             pass
 
-    def onScreensaverActivated(self):
-        print 'XBian : on saver'
-        self.inScreenSaver = True
-
-        if self.enableauto or self.StopRequested:
+    def doRefresh(self, force=False):
+        print 'XBian : on refresh (%s)' % 'Forced' if force else 'on saver'
+        self.refreshNext = self.eventTime + timedelta(hours=3)
+        if not self.forceRefresh and (self.enableauto or self.StopRequested):
             return
 
-        if not xbmc.Player().isPlaying() and self.selfCheck and (getSetting('lastupdatecheck') ==
-                                                                 None or getSetting('lastupdatecheck') < (datetime.now() - timedelta(days=self.deltaCheck))):
+        if self.forceRefresh or ((force or not xbmc.Player().isPlaying()) and self.selfCheck and (getSetting('lastupdatecheck') ==
+                                                                 None or getSetting('lastupdatecheck') < (self.eventTime - timedelta(days=self.deltaCheck)))):
+
+            setSetting('lastupdatecheck', self.eventTime)
+            print 'XBian : running internal database updates (%s)' % self.forceRefresh
+
+            # flush package cache if needed and refresh
+            pkglist = xbianConfig('packages', 'list', cache=True)
+            pkglistnc = xbianConfig('packages', 'list')
+            if pkglistnc[0] == '-3' or str(pkglist) != str(pkglistnc):
+                print 'XBian : cleaning package cache'
+                pkglist = xbianConfig('packages', 'updatedb', forceclean=True)
+                if pkglist and pkglist[0] == '1':
+                    pkglist = xbianConfig('packages', 'list', forcerefresh=True)
+                    self.forceRefresh = False
+            else:
+                self.forceRefresh = False
+
+            if pkglist and pkglist[0] != '-3':
+                for pkg in pkglist:
+                    xbianConfig('packages', 'list', pkg.split(',')[0], forcerefresh=True)
+
             # check if new upgrades avalaible
             rc = xbianConfig('updates', 'list', 'packages', forcerefresh=True)
             if rc and rc[0] == '-3':
@@ -64,98 +163,225 @@ class upgrade(service):
                     rc = xbianConfig('updates', 'list', 'packages', forcerefresh=True)
 
             if rc and rc[0] and len(rc[0]) > 2:
-                self.packageUpdate = True
+                self.updatesAvailable = True
                 print 'XBian : new updates available'
+            else:
+                self.updatesAvailable = False
+                if getSetting('hide.updates') != 1:
+                    self.hideUpdatesAvailable = False
+        print 'XBian : on refresh done'
 
-            # refresh also package cache
-            pkglist = xbianConfig('packages', 'list', forcerefresh=True)
-            if pkglist and pkglist[0] == '-3':
-                pkglist = xbianConfig('packages', 'updatedb')
-                if pkglist and pkglist[0] == '1':
-                    pkglist = xbianConfig('packages', 'list', forcerefresh=True)
 
-            if pkglist and pkglist[0] != '-3':
-                for pkg in pkglist:
-                    xbianConfig('packages', 'list', pkg.split(',')[0], forcerefresh=True)
+    def onScreensaverActivated(self):
+        print 'XBian : on saver'
+        self.inScreenSaver = True
+        self.eventTime = datetime.now()
+        self.doRefresh()
 
-            setSetting('lastupdatecheck', datetime.now())
-
-        print 'XBian : on saver END'
+    def onScreensaverDeactivated(self):
+        print 'XBian : on saver deactivated'
+        self.inScreenSaver = False
+        self.loop = 30
 
     def showRebootDialog(self):
         print 'XBian : show reboot dialog'
-
-        if self.inScreenSaver or os.path.isfile('/tmp/.xbian_config_python') or self.StopRequested:
-            return
-
-        stillrunning = xbianConfig('updates', 'progress')
-        if stillrunning and stillrunning[0] == '1':
-            return
-
-        self.rebootNeeded = False
-
         if xbmcgui.Dialog().yesno(
                 'XBian-config',
                 _('A reboot is required. Do you want to reboot now?')):
             xbmc.executebuiltin("XBMC.Restart()")
-        else:
-            self.rebootNoCheck = True
+        self.rebootPending = False
 
-        print 'XBian : show reboot dialog END'
+    def showHint(self, header, message, hint):
+        print 'XBian : show hint (%s)' % hint
+        if getSetting('advancedmode') == '1':
+            if xbmcgui.Dialog().yesno(
+                    header,
+                    message,'',
+                    _('Never show this hint again?')):
+                setSetting(hint, True)
+        else:
+            xbmcgui.Dialog().ok(header, message)
+        return True
+
+    def showMessage(self, header, messages):
+        print 'XBian : show message (%s)' % messages
+        xbmcgui.Dialog().ok(header, messages[0], messages[1], messages[2])
+        return True
 
     def onIdle(self):
-        if self.rebootNoCheck or os.path.isfile('/tmp/.xbian_config_python') or self.rebootNeeded:
+        self.eventTime = datetime.now()
+
+        # for those one who deactivate its screensaver, force check for refresh every 3 hours
+        if self.forceRefresh or self.refreshNext < self.eventTime:
+            self.doRefresh(True)
             return
 
-        rebootneeded = xbianConfig('reboot')
-        if rebootneeded and rebootneeded[0] == '1':
-            self.rebootNeeded = True
-            self.showRebootDialog()
+        if self.settingsUpdated:
+            xbmc.executebuiltin('Notification(' + _('Service') + ' XBianWorker' + ', ' + _('%s successfully updated') % (_('Service'),) +')')
+            self.settingsUpdated = False
 
-    def onScreensaverDeactivated(self):
-        print 'XBian : on saver deactivated'
-        if self.StopRequested:
+        if self.inScreenSaver or self.StopRequested or (not self.notifyWhenBusy and xbmc.Player().isPlaying()):
             return
 
-        self.inScreenSaver = False
+        if self.msgcount > 0:
+            self.showMessage(self.msgheader[self.msgdisp], self.msgcontent[self.msgdisp])
+            self.msgdisp += 1
+            if self.msgdisp < self.msgcount:
+                self.loop = 10
+            else:
+                self.msgcount = 0
+                self.msgdisp = 0
+            return
 
-        if self.packageUpdate:
-            xbmc.executebuiltin("Notification(Packages Update,Some XBian package can be updated)")
-            self.packageUpdate = False
+        if os.path.isfile('/tmp/.xbian_config_python'):
+            return
 
-        print 'XBian : on saver deactivated END'
+        if self.rebootPending:
+            rebootneeded = xbianConfig('reboot')
+            if rebootneeded and rebootneeded[0] == '1':
+                stillrunning = xbianConfig('updates', 'progress')
+                if stillrunning and stillrunning[0] == '0':
+                    self.showRebootDialog()
+                    return
+
+        if not self.hideUpgradeNotify and self.upgradePending:
+            rc = xbianConfig('updates', 'distupgrade', 'query')
+            if rc and rc[0] <> '0':
+                self.hideUpgradeNotify = self.showHint( _('Update') + ': ' + _('Distribution'), _('A distribution upgrade to %s is available') % (rc[0]), 'hide.upgradenotify')
+            else:
+                self.hideUpgradeNotify = False
+            self.upgradePending = False
+            return
+
+        if not self.hideUpdatesAvailable and self.updatesAvailable:
+            self.hideUpdatesAvailable = self.showHint( _('Package') + ' ' + _('Update'), _('Some XBian package can be updated'), 'hide.updates')
+            self.updatesAvailable = False
+            return
+
+        if not self.hideImageWarning and self.xbiancopyDate < (self.eventTime - self.timeImageDiff):
+            self.hideImageWarning = self.showHint(_('System backup'), _('This type of backup has not been made for a long time!'), 'hide.xbiancopy')
+            return
+
+        if not self.hideHomeWarning and self.backuphomeDate < (self.eventTime - self.timeHomeDiff) and self.xbiancopyDate < (self.eventTime - self.timeImageDiff):
+            self.hideHomeWarning = self.showHint(_('Home backup'), _('This type of backup has not been made for a long time!'), 'hide.backuphome')
+            return
+
+    def onStatusChanged(self, status, file=None):
+        print 'XBian : on status changed (%s,%s)' % (status,file)
+        if status == 'setting':
+            self.actualizeSettings(True)
+        elif status == 'reboot':
+            self.rebootPending = True
+        elif status == 'upgrade':
+            self.upgradePending = True
+        elif status == 'noreboot':
+            self.rebootPending = False
+        elif status == 'refresh':
+            self.forceRefresh = True
+        elif status == 'msg4kodi':
+            xbmc.sleep(500)
+            fd = False
+            try:
+                fd = open(file, "r")
+                i = 0
+                self.msgheader[self.msgcount] = ''
+                for line in fd:
+                    line = line.rstrip()
+                    if line[0] == '#':
+                        msgno = int(line[1:]) - 1
+                        for msg in msgs[msgno]:
+                            if len(msg) == 0:
+                                msg = ' '
+                            if i == 0:
+                                self.msgheader[self.msgcount] = msg
+                                self.msgcontent[self.msgcount] = [ '', '', '' ]
+                            elif i < 4:
+                                self.msgcontent[self.msgcount][i-1] = msg
+                            i += 1
+                        self.msgcount += 1
+                        i = 0
+                    else:
+                        if len(line) == 0:
+                            line = ' '
+                        if i == 0:
+                            self.msgheader[self.msgcount] = line
+                            self.msgcontent[self.msgcount] = [ '', '', '' ]
+                            i += 1
+                        elif line == '$':
+                            self.msgcount += 1
+                        elif i < 4:
+                            self.msgcontent[self.msgcount][i-1] = line
+                            i += 1
+                self.loop = 10
+            except:
+                print 'XBian : error reading message(s) from %s' % (file)
+            if fd:
+                fd.close()
+            try:
+                os.remove(file)
+            except:
+                pass
+        elif status == 'xbiancopy':
+            self.xbiancopyDate = datetime.fromtimestamp(os.path.getmtime(file))
+            if os.path.getsize(file) > 0:
+                self.xbiancopyDone = True
+            self.actualizeSettings()
+            if getSetting('hide.' + status) != 1:
+               self.hideImageWarning = False
+        elif status == 'backuphome':
+            self.backuphomeDate = datetime.fromtimestamp(os.path.getmtime(file))
+            if os.path.getsize(file) > 0:
+                self.backuphomeDone = True
+            self.actualizeSettings()
+            if getSetting('hide.' + status) != 1:
+                self.hideHomeWarning = False
+        elif status == 'hide.xbiancopy':
+            if getSetting(status) == 1:
+                self.hideImageWarning = True
+            else:
+                self.hideImageWarning = False
+        elif status == 'hide.backuphome':
+            if getSetting(status) == 1:
+                self.hideHomeWarning = True
+            else:
+                self.hideHomeWarning = False
+        elif status == 'hide.upgradenotify':
+            if getSetting(status) == 1:
+                self.hideUpgradeNotify = True
+            else:
+                self.hideUpgradeNotify = False
+        elif status == 'hide.updates':
+            if getSetting(status) == 1:
+                self.hideUpdatesAvailable = True
+            else:
+                self.hideUpdatesAvailable = False
+                rc = xbianConfig('updates', 'list', 'packages')
+                if rc and rc[0] and len(rc[0]) > 2:
+                    self.updatesAvailable = True
 
     def onStart(self):
         # check is packages is updating
         if os.path.isfile('/var/lock/.packages'):
             if xbianConfig('updates', 'progress')[0] == '1':
-                dlg = dialogWait('XBian Update', 'Please wait while updating')
+                dlg = dialogWait('XBian' + ' ' +_('Update'), _('Please wait while updating'))
                 dlg.show()
                 while not self.StopRequested and xbianConfig('updates', 'progress')[0] == '1':
                     xbmc.sleep(300)
                 dlg.close()
                 if self.StopRequested:
                     return
-            xbmc.executebuiltin("Notification(%s,%s)" %
-                                ('Package Update', 'Updates installed successfully'))
+            xbmc.executebuiltin("Notification(%s, %s)" % (_('Package') + ' ' + _('Update'), _('Updates installed successfully')))
             os.remove('/var/lock/.packages')
 
         if xbianConfig('updates', 'progress')[0] != '1':
             setvisiblecondition('aptrunning', False)
 
-        # for those one who deactivate its screensaver, force check every deltaCheck days
-        if self.selfCheck and (getSetting('lastupdatecheck') == None or getSetting(
-                'lastupdatecheck') < datetime.now() - timedelta(days=self.deltaCheck)):
-            print 'XBian : screensaver is disabled, running internal updates'
-            self.onScreensaverActivated()
-            self.onScreensaverDeactivated()
-
         while not self.StopRequested:  # End if XBMC closes
-            self.x = 0
-            while not self.StopRequested and self.x < 600:
-                xbmc.sleep(500)  # Repeat (ms)
-                self.x = self.x + 1
-
             self.onIdle()
+            if self.loop == 0:
+                self.loop = 600
+            while not self.StopRequested and self.loop > 0:
+                xbmc.sleep(500)  # Repeat (ms)
+                self.loop = self.loop - 1
 
-        print 'XBian : upgrade service finished'
+        print 'XBian : xbianworker service finished'
